719914a9f00f2f7f45f0500306305f60294f5184
Alena.Kygai <alena.kygai@gmail.com>
2022-10-06 17:09:16 +0300

    data transfer and use middleware thunk

--- src/components/ToDoContainer/ToDoContainer.js
+++ src/components/ToDoContainer/ToDoContainer.js
@@ -1,48 +1,25 @@
 import { useSelector,useDispatch } from "react-redux";
 import ToDoForm from "../ToDoForm/ToDoForm";
 import ToDoList from '../ToDoList/ToDoList';
+import { addTask, removeTask, isDoneTask } from "../actions";
+
 
 
 const ToDoContainer = () => {
   const dispatch = useDispatch();
   const todos = useSelector(state => state.todos);
+  
+  const handleAddTask = (task) => dispatch(addTask(task))
 
-    const addTask = (task) => dispatch ({
-      type: 'ADD_TASK',
-      payload:{
-      id: new Date().getTime(),
-      task,
-      isDone: false,
-    }
-  })
+  const handleremoveTask = (id) => dispatch(removeTask(id))
 
-  const removeTask = (id) => {
-    dispatch({
-      type: 'REMOVE_TASK',
-      payload:todos.filter(todo => todo.id!==id),
-    })
-  }
-
-  const isDoneTask = (id) => {
-    dispatch({
-      type: 'CHANGE_STATUS_TASK',
-      payload:todos.map((todo) => {
-        if(todo.id===id){
-          return  ({
-            ...todo,
-            isDone:!todo.isDone
-          })
-        }
-        return todo;
-      }) 
-    })
-  }
+  const handleisDoneTask = (id) => dispatch(isDoneTask(id))
 
   return(
     <div className="App">
       <header>ADD TASK: {todos.length}</header>
-      <ToDoForm addTask={addTask} />
-      <ToDoList todos={todos} removeTask={removeTask} isDoneTask={isDoneTask}/>
+      <ToDoForm addTask={handleAddTask} />
+      <ToDoList todos={todos} removeTask={handleremoveTask} isDoneTask={handleisDoneTask}/>
     </div>
   )
 }
